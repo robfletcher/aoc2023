5886d3b6e18c02d9d95f81af620a7c8cad82f135
Rob Fletcher <rfletcher@netflix.com>
2023-12-05 11:20:54 -0800

    day 5 part 2 brute force

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,3 +1,9 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlin.math.max
+import kotlin.math.min
+
 class Day05 : Puzzle {
   override fun test() {
     val input = """
@@ -36,41 +42,96 @@ class Day05 : Puzzle {
       56 93 4""".trimIndent()
 
     assert(part1(input.lineSequence()) == 35L)
+    assert(part2(input.lineSequence()) == 46L)
   }
 
   override fun part1(input: Sequence<String>): Long {
     val lines = input.toList()
     val seeds = Regex("""\d+""").findAll(lines.first()).map(MatchResult::value).map(String::toLong)
+    val transforms = parseInput(lines.drop(1))
+
+    return seeds.minOf { transforms.resolve("seed", it) }
+  }
+
+  override fun part2(input: Sequence<String>): Long {
+    val lines = input.toList()
+    val seeds = Regex("""(\d+) (\d+)""").findAll(lines.first()).map(MatchResult::groupValues).map { (_, start, length) ->
+      start.toLong()until(start.toLong() + length.toLong())
+    }
+    val transforms = parseInput(lines.drop(1))
+
+//    seeds.fold(emptyList<LongRange>()) { list, r2 ->
+//      when {
+//        list.isEmpty() -> listOf(r2)
+//        list.any { it.first <= r2.first && it.last >= r2.last } -> list
+//        else -> list + listOf(r2)
+//      }
+//    }
+
+    return seeds.toList().run {
+      runBlocking {
+        map { seedRange ->
+          async(Dispatchers.Default) {
+            println("Searching ${seedRange.first}..${seedRange.last}")
+            seedRange.minOf { seed ->
+              transforms.resolve("seed", seed)
+            }
+          }
+        }
+          .minOf { it.await() }
+      }
+    }
+  }
+
+  private fun LongRange.merge(other: LongRange): List<LongRange> {
+    return if (first <= other.first && last >= other.last) {
+      // this contains other
+      listOf(this)
+    } else if (first >= other.first && last <= other.last) {
+      // other contains this
+      listOf(other)
+    } else if (last < other.first || first > other.last) {
+      // separate ranges
+      listOf(this, other)
+    } else {
+      listOf(min(first, other.first)..max(last, other.last))
+    }
+  }
+
+//  private suspend fun Map<String, Transform>.search(seeds: Sequence<Long>) =
+//    coroutineScope {
+//      seeds.toList()
+//        .map { seed ->
+//          async {
+//            resolve("seed", seed)
+//          }
+//        }
+//        .map { it.await() }
+//    }
+
+  private fun parseInput(lines: List<String>): Map<String, Transform> {
     val transforms = mutableMapOf<String, Transform>()
     var lastSrc = "seed"
-    lines.drop(1).forEach { line ->
+    lines.forEach { line ->
       when {
         line.matches(Regex("""(\w+)-to-(\w+) map:""")) -> {
           lastSrc = line.substringBefore("-to-")
           transforms[lastSrc] = Transform(src = lastSrc, dest = line.substringAfter("-to-").substringBefore(' '))
-          println("found $lastSrc to ${transforms[lastSrc]!!.dest}")
         }
 
         line.isNotBlank() -> Regex("""(\d+) (\d+) (\d+)""").find(line)!!.groupValues.drop(1).map(String::toLong).let { (destStart, srcStart, length) ->
           transforms[lastSrc] = transforms[lastSrc]!!.run {
-            copy(mappings = mappings + Mapping(LongRange(srcStart, srcStart + length), destStart)).also {
-              println(it)
-            }
+            copy(mappings = mappings + Mapping(srcStart, srcStart + length, destStart))
           }
         }
       }
     }
-
-    return seeds.minOf { seed ->
-      transforms.resolve("seed", seed)
-    }
+    return transforms
   }
 
   fun Map<String, Transform>.resolve(type: String, value: Long): Long {
     val transform = this[type]
-    return if (transform == null) value else resolve(transform.dest, transform.translate(value)).also {
-      println("$type $value = ${transform.dest} $it")
-    }
+    return if (transform == null) value else resolve(transform.dest, transform.translate(value))
   }
 
   data class Transform(
@@ -79,14 +140,15 @@ class Day05 : Puzzle {
     val mappings: Set<Mapping> = emptySet()
   ) {
     fun translate(srcValue: Long): Long {
-      val mapping = mappings.find { srcValue in it.srcRange }
+      val mapping = mappings.find { srcValue >= it.srcStart && srcValue < it.srcEnd }
       return if (mapping == null) srcValue
-      else mapping.srcRange.indexOf(srcValue) + mapping.destStart
+      else srcValue - mapping.srcStart + mapping.destStart
     }
   }
 
   data class Mapping(
-    val srcRange: LongRange,
+    val srcStart: Long,
+    val srcEnd: Long,
     val destStart: Long
   )
 }
