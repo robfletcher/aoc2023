cf10b596c00322ea004712e34e6a0793ba1029ed
Rob Fletcher <rfletcher@netflix.com>
2023-12-08 23:03:19 -0800

    day 9 part 2

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -6,6 +6,7 @@ class Day09 : Puzzle {
       10 13 16 21 30 45""".trimIndent()
 
     assert(part1(input.lineSequence()) == 114)
+    assert(part2(input.lineSequence()) == 2)
   }
 
   override fun part1(input: Sequence<String>) =
@@ -13,10 +14,22 @@ class Day09 : Puzzle {
       line.split(' ').map(String::toInt).findNext()
     }
 
+  override fun part2(input: Sequence<String>) =
+    input.sumOf { line ->
+      line.split(' ').map(String::toInt).findPrevious()
+    }
+
   private fun Iterable<Int>.findNext(): Int =
     if (all { it == 0 }) {
       0
     } else {
       last() + windowed(2).map { (a, b) -> b - a }.findNext()
+    }
+
+  private fun Iterable<Int>.findPrevious(): Int =
+    if (all { it == 0 }) {
+      0
+    } else {
+      first() - windowed(2).map { (a, b) -> b - a }.findPrevious()
     }
 }
